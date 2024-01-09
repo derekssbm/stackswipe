cee22d5183d1d7180dc1c0c0374eda6c1c8ef519
derekssbm <88495991+derekssbm@users.noreply.github.com>
2024-01-09 19:26:01 +0100

    initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
+import daisyui from 'daisyui'; // Import DaisyUI
 
 const config: Config = {
   content: [
@@ -10,11 +11,11 @@ const config: Config = {
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
   },
-  plugins: [],
+  plugins: [daisyui], // Add DaisyUI plugin
 }
-export default config
+
+export default config;
